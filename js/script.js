c8fb95ba5ac0672a677f1d658da7ac8f449b1910
filipeomendes <filipedeoliveiramendes@gmail.com>
2023-04-27 22:51:03 -0300

    Ternário, diferença entre VAR/LET/CONST e defer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-alert("Hello World!");
+
+// var nome = "Luizinho";
+// let nome2 = "Vitinho";
+// const nome3 = "Miguelito";
+
+// // console.log(nome);
+// // console.log(nome2);
+// // console.log(nome3);
+
+// if(nome != ""){
+//     let nome = "Nicoly";
+// }
+
+// console.log("Qual nome vai ser impresso aqui?");
+// console.log(nome);
+
+//Quando declaramos uma variável com var
+//o JavaScript aplica um processo que chamamos de HOISTING
+//O HOISTING ou elevação, coloca todas as variáveis do tipo var no topo do programa.
+//Isso faz com que se existirem variáveis com mesmo nome, elas se sobrepõem.
+
+// let nr1 = 10;
+// let nr2 = 5;
+
+// //IT TERNARIO - DESCOBRIR SE O NUMERO É PAR OU IMPAR
+// let resultado = !(nr1 % nr2) ? "PAR" : "IMPAR";
+
+// console.log(resultado);
+
+
+//Recuperar um elemento do HTML com a função getElementById(parâmetro)
+const mn = document.getElementById("menu");
+console.log(mn.textContent);
