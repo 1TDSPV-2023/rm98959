53a648246dc5bd0ec5b68dd99e4d6bacfafdd922
filipeomendes <filipedeoliveiramendes@gmail.com>
2023-05-02 23:00:02 -0300

    Arrays iniciados

--- js/script.js
+++ js/script.js
@@ -29,5 +29,53 @@
 
 
 //Recuperar um elemento do HTML com a função getElementById(parâmetro)
-const mn = document.getElementById("menu");
-console.log(mn.textContent);
+//const mn = document.getElementById("menu");
+//console.log(mn.textContent);
+
+//Recuperando uma collection de elementos...
+const aElements = document.getElementsByTagName("a");
+const aElementsArray = [...aElements];
+
+aElementsArray.forEach((a)=>{
+    console.log(a);
+});
+
+////Declarando um array em Javascript
+//let nr1 = [1,2,3,4,5];
+//let nr2 = [6,7,8,9,10];
+////let nr3 = [nr1, nr2];
+//console.log(nr1);
+//console.log(nr2);
+
+////OPERADOR SPREAD...
+
+//let nr3 = [...nr1,...nr2];
+//console.log("NOVO ARRAY: " + nr3);
+
+//nr3.forEach( (numero)=>{
+//    console.log("ITEM DO ARRAY: " + numero)
+//} );
+
+
+
+
+//Exercícios
+//1 - Recupere uma coleção de imagens da página e imprima o
+
+const imgElements = [...document.getElementsByTagName("img")];
+//const imgElementsArray = [...imgElements];
+//console.log(imgElements);
+
+//IMPRIMINDO O ATRIBUTO SRC do elemento com o forEach
+imgElements.forEach((img)=>{
+    console.log(`Path das imagens : ${img.src}`);
+} );
+
+
+
+const botao = document.querySelector("div > button");
+console.log(botao);
+
+botao.addEventListener("clicl" , ()=>{
+    alert("TCHAU!");
+} );
